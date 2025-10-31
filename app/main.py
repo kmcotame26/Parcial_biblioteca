803ff0cf1c85bf09f2398823ff583f74d4d1fb74
kmcotame26 <kmcotame26@ucatolica.edu.co>
2025-10-31 08:05:34 -0500

    Porfin jaja

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from fastapi.responses import RedirectResponse, Response
 from .routers import autores, libros
 
-app = FastAPI(title="Sistema de Gestión de Biblioteca para Parcial")
+app = FastAPI(title="Gestión de Biblioteca Karol")
 
 app.include_router(autores.router)
 app.include_router(libros.router)
